87f4ed9abeae90ab0dca920d29ad016616ef954c
hrgdavor <hrgdavor@gmail.com>
2015-10-08 22:06:12 +0200

    no need for wrapper funtion

--- src/joinUrl.js
+++ src/joinUrl.js
@@ -1,19 +1,16 @@
-(function(mi2){
 
-	mi2.joinUrl = function(pars){
-		if(!pars) return "";
-		// some properties can be arrays (when encoding multiple checkbox data)
-		var p, val, arr = [], e=encodeURIComponent; 
-		for(p in pars){
-			val = pars[p];
-			if(val == null) continue;
-			if(val instanceof Array)
-				for(var p2 in val) 
-					arr.push(e(p)+"="+e(val[p2]));
-			else
-				arr.push(e(p)+"="+e(pars[p]));
-		}
-		return arr.join('&');
-	};
-
-}(mi2JS));
+mi2JS.joinUrl = function(pars){
+	if(!pars) return "";
+	// some properties can be arrays (when encoding multiple checkbox data)
+	var p, val, arr = [], e=encodeURIComponent; 
+	for(p in pars){
+		val = pars[p];
+		if(val == null) continue;
+		if(val instanceof Array)
+			for(var p2 in val) 
+				arr.push(e(p)+"="+e(val[p2]));
+		else
+			arr.push(e(p)+"="+e(pars[p]));
+	}
+	return arr.join('&');
+};
--- src/parseUrl.js
+++ src/parseUrl.js
@@ -1,27 +1,24 @@
-(function(mi2){
 
-	mi2.parseUrl = function(query){
-		if(!query || query == '') return {};
-		// some properties can be arrays (when encoding multiple checkbox data)
-		var ret = {}; 
-		var match, name, val,
-		    pl     = /\+/g,  // Regex for replacing addition symbol with a space
-		    search = /([^&=]+)=?([^&]*)/g,
-		    decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); };
+mi2JS.parseUrl = function(query){
+	if(!query || query == '') return {};
+	// some properties can be arrays (when encoding multiple checkbox data)
+	var ret = {}; 
+	var match, name, val,
+	    pl     = /\+/g,  // Regex for replacing addition symbol with a space
+	    search = /([^&=]+)=?([^&]*)/g,
+	    decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); };
 
-		while (match = search.exec(query)){
-			name = decode(match[1]);
-			val = decode(match[2]);
-			if(ret[name]){
-				if(ret[name].push){ // already array
-					ret[name].push(val);
-				}else{ // second hit, not yet array
-					ret[name] = [ret[name], val];
-				}
-			}else
-				ret[name] = val;
-		}
-  		return ret;
-	};
-
-}(mi2JS));
+	while (match = search.exec(query)){
+		name = decode(match[1]);
+		val = decode(match[2]);
+		if(ret[name]){
+			if(ret[name].push){ // already array
+				ret[name].push(val);
+			}else{ // second hit, not yet array
+				ret[name] = [ret[name], val];
+			}
+		}else
+			ret[name] = val;
+	}
+		return ret;
+};
