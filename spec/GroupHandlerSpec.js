22efc57a6a3363fe5a4c80e3ac947d46b4c0fe0c
hrgdavor <hrgdavor@gmail.com>
2015-10-24 17:23:58 +0200

    move GroupHandler to Group

--- spec/GroupHandlerSpec.js
+++ spec/GroupHandlerSpec.js
@@ -1,13 +1,13 @@
-describe( 'GroupHandler.js', function () { 
+describe( 'Group.js', function () { 
 	var mi2 = mi2JS;
 
 	it('/ fetch + callback', function (){
-		var v1 = mi2.GroupHandler({});
-		var v2 = new mi2.GroupHandler({});
+		var v1 = mi2.Group({});
+		var v2 = new mi2.Group({});
 
 		// check if function is implemented so that it returns "new func(...)" when called without "new"
-		expect( v1 instanceof mi2.GroupHandler ).toBeTruthy();
-		expect( v2 instanceof mi2.GroupHandler ).toBeTruthy();
+		expect( v1 instanceof mi2.Group ).toBeTruthy();
+		expect( v2 instanceof mi2.Group ).toBeTruthy();
 	});
 
 });
