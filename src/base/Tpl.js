ad5c5768c18466e9d2e60086fa3614da21adb756
hrgdavor <hrgdavor@gmail.com>
2015-12-20 21:32:51 +0100

    support forwarding template data

--- src/base/Tpl.js
+++ src/base/Tpl.js
@@ -4,17 +4,34 @@ mi2JS.comp.add('base/Tpl', 'Base', '',
 // component initializer function that defines constructor and adds methods to the prototype 
 function(proto, superProto, comp, superComp){
 
-	var mi2 = mi2JS;
+    var mi2 = mi2JS;
 
-	proto.isTransitive = function(){ return true; };
+    proto.isTransitive = function(){ return true; };
 
-	proto.construct = function(el, tpl, parent){
-		superProto.construct.call(this, el, tpl, parent);
-		this.template = mi2.loadTemplate(el);
-	};
+    proto.construct = function(el, tpl, parent){
+        superProto.construct.call(this, el, tpl, parent);
+        this.initTemplate();
+    };
 
-	proto.setValue = function(data){
-		this.template.setValue(data);
-	};
+    proto.initTemplate = function(){
+        this.template = mi2.loadTemplate(this.el);
+        this.templateFwd = [];
+        if(!this.children) return;
+        var count = this.children.length;
+        for(var i=0; i<count; i++){
+            if(this.children[i].hasAttr('fwd-tpl-data')){
+                if(this.children[i].setValue)
+                    this.templateFwd.push(this.children[i]);
+            }
+        }        
+    };
+
+    proto.setValue = function(data){
+        this.template.setValue(data);
+        var count = this.templateFwd.length;
+        for(var i=0; i<count; i++){
+            this.templateFwd[i].setValue(data);
+        }
+    };
 
 });
