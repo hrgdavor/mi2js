4b679173c57988a956015b9c9f0f6258fa9572d5
hrgdavor <hrgdavor@gmail.com>
2016-03-02 16:24:07 +0100

    new Button version
    with tempalte support and multiple clickable elements inside, just add
    event / action attributes and event is fired on click

--- src/base/Button.js
+++ src/base/Button.js
@@ -1,49 +1,64 @@
+/*
+Combination of button functionality with variable expansion support.
 
-mi2JS.comp.add('base/Button', 'Base', '',
+*/
+
+mi2JS.comp.add('base/Button', 'base/Tpl', '',
 
 // component initializer function that defines constructor and adds methods to the prototype 
 function(proto, superProto, comp, superComp){
 
+	proto.minClickDelay = 300;
+
 	proto.initTemplate = function(){
 		superProto.initTemplate.call(this);
-		var el = this.el;
+		this.listen(this.el,'click');
+	};
 
-		this.lastClick = 0;
-		this.listen(el,"click",function(evt){
-			if(evt.which != 1) return; // only left click
+	proto.on_click = function(evt){
+		if(evt.which != 1) return; // only left click
+
+		var evtNames = [], actions = [], comp, enabled = true;
+		var el = evt.target;
+		while(true){
+			if(this.cancelClick(el)) return;
+
+			if(el.hasAttribute('event')) evtNames.push(el.getAttribute('event'));
+			if(el.hasAttribute('action')) actions.push(el.getAttribute('action'));
+
+			if(el == this.el) break;
+			el = el.parentNode;
+		}
+
+		if(this.__propName) actions.push(this.__propName);
+
+		if(evtNames.length && this.parent.fireEvent){
+			var now = Date.now();
+			if(this._lastClick && now - this._lastClick < this.minClickDelay) return;	
+			this._lastClick = now;
+
+			this.parent.fireEvent(evtNames[0],{
+				action:actions[0],
+				actions:actions,
+				events:evtNames,
+				domEvent: evt,
+				target: evt.target,
+				src:this,
+				eventFor: 'parent'
+			});
 			evt.stop();
-			if(this.isEnabled() && this.parent.fireEvent){
-				var now = new Date().getTime();
-				if(now -this.lastClick > 300){ // one click per second
-					this.parent.fireEvent(this.getEvent(), {
-						action: this.getAction(), 
-						button:this, 
-						domEvent: evt, 
-						src:this,
-						eventFor: 'parent'
-					}); 
-					this.lastClick = now;
-				} 
-			}
-		});
-
+			return false;
+		}
 	};
 
-	proto.getEvent = function(){
-		return this.attrDef("event", "action");
+	proto.cancelClick = function(el){
+		
+		// if disabled at any level 
+		if(el.hasAttribute(mi2JS.disabledAttribute)) return true;
+		
+		// do not mess with other components
+		comp = el.getAttribute('as');
+		return (comp && el != this.el && !(comp == 'base/Tpl' || comp =='Base' )); 
 	};
 
-	proto.getAction = function(){
-		return this.attrDef("action", this.__propName);
-	};
-
-	proto.setValue = function(value){
-		this.el.value = value;
-	};
-
-	proto.getValue = function(value){
-		return this.el.value;
-	};
-
-
-}); 
+});
