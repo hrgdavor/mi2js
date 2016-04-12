52c58b31e43f841c0c025f03c5f4660ca7c6198e
hrgdavor <hrgdavor@gmail.com>
2016-04-12 17:26:37 +0200

    inline template support for base/Tpl

--- src/base/Tpl.js
+++ src/base/Tpl.js
@@ -10,8 +10,32 @@ function(proto, superProto, comp, superComp){
 
     proto.parseChildren = function(){
         superProto.parseChildren.call(this);
+        this.loadTemplate();
+    };
 
-        this.template = mi2.loadTemplate(this.el);
+    // in case of inline template
+    proto.initTemplate = function(){
+        superProto.initTemplate.call(this);
+        if(!this.template) this.loadTemplate();
+    };
+
+    proto.setValue = function(data){
+        if(!this.template) {
+            var m = 'template not initialized yet';
+            console.error(m);
+            console.log(m, this.el,this.__initialized, this.el.innerHTML, mi2.loadTemplate(this.el, this));
+            if(!this.template)
+                this.template = mi2.loadTemplate(this.el, this);
+        }
+        this.template.setValue(data);
+        var count = this.templateFwd.length;
+        for(var i=0; i<count; i++){
+            this.templateFwd[i].setValue(data);
+        }
+    };
+
+    proto.loadTemplate = function(){
+        this.template = mi2.loadTemplate(this.el, this);
         this.templateFwd = [];
 
         if(!this.children) return;
@@ -23,14 +47,6 @@ function(proto, superProto, comp, superComp){
                     this.templateFwd.push(this.children[i]);
             }
         }        
-    };
-
-    proto.setValue = function(data){
-        this.template.setValue(data);
-        var count = this.templateFwd.length;
-        for(var i=0; i<count; i++){
-            this.templateFwd[i].setValue(data);
-        }
     };
 
 });
