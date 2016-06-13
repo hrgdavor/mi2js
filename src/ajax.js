1fd1f74ecda2b1e09ae7a2ee596c09360379072e
hrgdavor <hrgdavor@gmail.com>
2016-06-13 14:18:23 +0200

    ajax timeout support

--- src/ajax.js
+++ src/ajax.js
@@ -1,9 +1,6 @@
 (function(mi2){
 
-	function MicroAjax(){
-		this.request = this.build();
-	}
-	var proto = MicroAjax.prototype;
+	var build;
 
 	var builders = [
 		function(){return new XMLHttpRequest();},
@@ -13,12 +10,24 @@
 	for(var i=0; i<builders.length; i++){
 		try{ 
 			builders[i](); 
-			proto.build = builders[i]; 
+			build = builders[i]; 
 			break;
 		}catch(e){}
 	}
 
-	proto.fetch	= function(_arg){
+
+	/** ajax request <br>
+		Parameters: single object with fields
+		@param {string} [method=GET] - HTTP method
+		@param {string|object} url - if object is provided then mi2JS.joinUrl will be called to gen url string 
+		@param {function} callback - callback function when data is received
+		@param {function} errback - callback if communication error occurs
+		@param {boolean} [async=false] - async / sync
+		@param {int} timeout - timeout in ms
+		@param {object} - The value of this provided for the call to callback and errback, check Function.prototype.call docs online
+	*/
+	mi2.ajax = function(_args){
+		xhr = build();
 		var args = mi2.update({ // defaults
 			method:'GET', 
 			self: this,
@@ -26,17 +35,17 @@
 			callback:function(){}, 
 			async:true, 
 			headers:{}
-		}, _arg);
+		}, _args);
 
 		args.method = args.method.toUpperCase();
 
-		this.request.open(args.method, args.url, args.async);
+		xhr.open(args.method, args.url, args.async);
 
 		if (args.method == 'POST' && !args.headers['Content-Type']) args.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-		for(var h in args.headers) this.request.setRequestHeader(h,args.headers[h]);
+		for(var h in args.headers) xhr.setRequestHeader(h,args.headers[h]);
 
 		var ret = null;
-		this.request.onreadystatechange = function(){
+		xhr.onreadystatechange = function(){
 			if(this.readyState == 4){
 				if(this.status == 200){
 					args.callback.call(args.self,this);
@@ -46,22 +55,16 @@
 			}
 		};
 
-		this.request.send(args.postData);
+		if(args.timeout){
+			xhr.timeout = args.timeout;		
+			xhr.ontimeout = function(){
+				if(args.errback) args.errback.call(args.self,this);
+			}
+		}
+
+		xhr.send(args.postData);
 
 		return ret;
-	};
-
-	/** ajax request <br>
-		Parameters: single object with fields
-		@param {string} [method=GET] - HTTP method
-		@param {string|object} url - if object is provided then mi2JS.joinUrl will be called to gen url string 
-		@param {function} callback - callback function when data is received
-		@param {function} errback - callback if communication error occurs
-		@param {boolean} [async=false] - async / sync
-		@param {object} - The value of this provided for the call to callback and errback, check Function.prototype.call docs online
-	*/
-	mi2.ajax = function(args){
-		return new MicroAjax().fetch(args);
 	};
 
 }(mi2JS));
