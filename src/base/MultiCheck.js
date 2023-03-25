18007ffb0591b6bb5df95b3f103e1f7afd140ec5
Davor Hrg <hrgdavor@gmail.com>
2023-03-25 19:33:44 +0100

    Update MultiCheck.js

--- src/base/MultiCheck.js
+++ src/base/MultiCheck.js
@@ -1,13 +1,13 @@
 mi2JS.addCompClass('base/MultiCheck', 'base/InputBase', '',
 
 // component initializer function that defines constructor and adds methods to the prototype 
-function(proto, superProto, comp, superComp){
-	
+function(proto, superProto, comp, mi2, h, t, filters){
+
 	var mi2 = mi2JS;
 
 	if(mi2JS.applyNwGroup){
 		mi2JS.applyNwGroup(proto, {setValue:1,getValue:1});
-	}else 
+	}else
 		mi2JS.mixin(comp,mi2JS.NWGroup);
 
 
@@ -69,8 +69,17 @@ function(proto, superProto, comp, superComp){
 			var id = isArray ? data[p].id : p;
 			var item = mi2.add(this.el, this.itemTpl);
 			item.dataAttr('id', id);
+			item.attr('icon', data[p].icon);
+			item.attr('x-title', data[p].title);
 			item.setText(   isArray ? (data[p].text || data[p].name)  : data[p]);
+			if(data[p].count) item.el.innerHTML = '<span class="count">'+data[p].count+'</span>'
+			if(data[p].disabled) item.attr('disabled', true);
 			this.items[id] = item;
+		}
+
+		if (!this.items || !Object.keys(this.items).length && this.attr('no-data-message')) {
+			let elem = mi2.add(this.el, {tag:'DIV'})
+			elem.setText(t(this.attr('no-data-message')))
 		}
 	};
 
@@ -88,5 +97,45 @@ function(proto, superProto, comp, superComp){
 
 		return ret;
 	};
+
+	proto.setRelated = function (component, fieldName) {
+		this.relatedComponent = component
+		this.relatedFieldName = fieldName
+		this.listen(component, 'change', evt => {
+			this.reloadData()
+		})
+	}
+
+	proto.setCacheKey = function(key) {
+		this.cackeKey = key
+	}
+
+	proto.reloadData = function() {
+		let data = MAIN_APP.entityCache.allLists[this.cackeKey]
+		if (!this.relatedComponent) {
+			this.setConfig(data)
+
+			return
+		}
+
+		const relatedValue = this.relatedComponent.getValue()
+		if (!relatedValue || (Array.isArray(relatedValue) && !relatedValue.length)) {
+			this.setConfig([])
+
+			return
+		}
+
+		data = UccUtil.filterOptions(data, this.relatedFieldName, relatedValue)
+		this.setConfig(data)
+	}
+
+  proto.markCustomValid = function (isValid) {
+    this.classIf('validation-error', !isValid)
+    this.classIf('required', !isValid)
+    this.attr('validation-error-message', isValid ? null : mi2.validationMessage({ message: 'required' }));
+  }
+
+	proto.markValidate = function() {
+	}
 
 });
